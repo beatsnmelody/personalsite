d5512ce6c653cc862745ba2b4e353bdb188a2a75
beatsnmelody <beatsnmelody1@gmail.com>
2023-12-11 18:26:59 -0600

    navbar started

--- my-project/src/components/Homepage.jsx
+++ my-project/src/components/Homepage.jsx
@@ -1,3 +1,5 @@
+import { NavBar } from "./NavBar";
+
 export default function Homepage() {
   <>
     <html data-theme="default"></html>
@@ -7,15 +9,28 @@ export default function Homepage() {
     <html data-theme="aqua"></html>
   </>;
   return (
-    <div>
-      <input
-        type="checkbox"
-        value="valentine"
-        className="toggle theme-controller"
-      />
-      <button className="btn btn-primary">One</button>
-      <button className="btn btn-secondary">Two</button>
-      <button className="btn btn-accent btn-outline">Three</button>
-    </div>
+    <>
+      <NavBar />
+      <div className="m-3 flex flex-row-reverse">
+        <input
+          type="checkbox"
+          value="valentine"
+          className="toggle theme-controller"
+        />
+        <button className="btn btn-primary">One</button>
+        <button className="btn btn-secondary">Two</button>
+        <button className="btn btn-accent btn-outline">Three</button>
+        <div>
+        <p className="text-accent text-5xl font-dhurjati p-3">
+          WELCOME TO THE ALWAYS-CREATIVE, EVER-INNOVATIVE MIND OF AL...
+        </p>
+        <p className="text-secondary text-3xl font-dhurjati p-1">
+          Formally called Alexandra Labaisse! This is the main site for all of
+          my projects, from art to web and game development, and maybe some
+          beats in the future...? Stay tuned!
+        </p>
+        </div>
+      </div>
+    </>
   );
 }
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
+    theme: {
+      extend:
+      {
+        fontFamily: {
+          'dhurjati': ['Dhurjati', 'normal']
+        },
+      },
+    },
     plugins: [require("daisyui")],
     daisyui: {
         themes: ["default", "retro", "valentine", "cyberpunk", "aqua"],
